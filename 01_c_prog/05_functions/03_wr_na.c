8e0107ee036c05d36744e4c5cbda73f5788ca7dd
lokigowdu <lokitejagowdu@gmail.com>
2023-04-05 13:09:25 +0530

    program

--- 01_c_prog/05_functions/03_wr_na.c
+++ 01_c_prog/05_functions/03_wr_na.c
@@ -8,7 +8,7 @@
  *
  * Application: 
  * Brief: 
- * Author: Natarajan S  ( natarajan.s@ruggedboard.com )
+ * Author: Lokesh S  ( natarajan.s@ruggedboard.com )
  * Title: Program Manager / Trainner
  * Last Modified Date: 19.09.2022
  *
